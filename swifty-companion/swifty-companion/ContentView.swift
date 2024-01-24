3dc95e2a082526fe904df2a20b39f37f97cb4469
Gregoire Drion <gregoire.drion@decathlon.com>
2024-01-24 19:23:13 +0100

    Initial commit

---
+++ swifty-companion/swifty-companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swifty-companion
+//
+//  Created by Grégoire DRION on 24/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swifty-companion/swifty-companion/swifty_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_companionApp.swift
+//  swifty-companion
+//
+//  Created by Grégoire DRION on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct swifty_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
